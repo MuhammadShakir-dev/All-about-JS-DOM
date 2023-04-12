34d6b456575ed8b5d6d6ad3229f11daf2712b7ca
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-04-12 22:12:44 +0500

    ✅ Main document targeting methods added.

---
+++ document2.js
@@ -0,0 +1,12 @@
+// let's have a look on main document targeting methods.
+
+document.getElementById("header");
+// if you want to get any id that you used in your HTML document than use this method.
+
+document.getElementByTagName("h1");
+// if you want to target any tag that you use in you html document then use this method.
+
+document.getElementByClassName("container");
+// if you want to target any class that you used in your html document then use this mehtod.
+
+//Note down one thing all these methods should be written in camel case.
